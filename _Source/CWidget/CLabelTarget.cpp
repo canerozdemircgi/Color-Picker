37c4865c279dbb0f8c7bc94dcf380921960a7ead
Caner Özdemir <canerozdemir.mail@gmail.com>
2026-04-08 20:02:03 +0300

    .

--- _Source/CWidget/CLabelTarget.cpp
+++ _Source/CWidget/CLabelTarget.cpp
@@ -15,15 +15,15 @@ const QByteArray COLOR_LIGHT{"#ffffff"_ba};
 const std::array<const QByteArray, 2> COLORS{COLOR_DARK, COLOR_LIGHT};
 
 }
-//
+
 CLabelTarget::CLabelTarget(QWidget* const parent, QCache<const QString, const QPixmap>* const cachePixmap) :
-	CLabelSvg(parent, {{.path = path}, {.cachePixmap = cachePixmap, .cacheColors = COLORS}})
+	CLabelSvg{parent, {{.path = path}, {.cachePixmap = cachePixmap, .cacheColors = COLORS}}}
 {
 }
 
 void CLabelTarget::setVisible(bool visible)
 {
 	if (visible)
-		this->setColor(CColor::luminance(this->parentWidget()->palette().color(QPalette::ColorRole::Button)) >= 0.5f ? COLOR_DARK : COLOR_LIGHT);
+		this->setColor(CColor::luminance(this->parentWidget()->palette().color(QPalette::ColorRole::Button)) > 0.5f ? COLOR_DARK : COLOR_LIGHT);
 	CLabelSvg::setVisible(visible);
 }
